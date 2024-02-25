c4c6bdd29735d18b19ffcb7a01335900ff88c009
rajamishra2209 <146928228+rajamishra2209@users.noreply.github.com>
2024-02-25 21:19:19 +0000

    conersions

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,15 @@
+let str1 ="hello"
+let str2 =" Raja"
+
+let str3= str1+str2
+console.log(str3);
+
+console.log(1 + "2"); //output = 12
+
+console.log("1" + 2); // output =12
+
+console.log(1 + 2 + "3"); // output = 33 cause first number is Integer so accordingly preference
+
+console.log(+true); // output = 1 
+console.log(+""); // output = 0
+
